cceccba441d2b094bade6fafabc8d9c64bd0009f
NoworNeverAndroid <divergentdev@gmail.com>
2015-08-26 17:01:06 +0200

    Initial commit

---
+++ app/src/main/java/es/com/blogspot/noworneverandroid/boxinsetlayout/MainActivity.java
@@ -0,0 +1,19 @@
+package es.com.blogspot.noworneverandroid.boxinsetlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
